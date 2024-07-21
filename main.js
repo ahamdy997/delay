4584835deac74d593a8913dc7432fffa0b8baaaa
Ahmed Hamdy <bme.ahmedhamdy@gmail.com>
2024-07-22 02:13:33 +0300

    a

--- main.js
+++ main.js
@@ -6,32 +6,32 @@
         deliverDelayHost.querySelector('.spinner').classList.add('d-none');
         clearTimeout(firstDelay);
     },500)
-    deliverDelayHost.querySelector('.layer').addEventListener('click', () => {
-        deliverDelayHost.querySelector('.spinner').classList.remove('d-none');
-        deliverDelayHost.querySelector('#thank-you-dialog').close();
-        deliverDelayHost.querySelector('#complain').close();
-        deliverDelayHost.querySelector('.layer').style.opacity = 0;
-        deliverDelayHost.querySelector('.layer').style.zIndex = -10;
+    // deliverDelayHost.querySelector('.layer').addEventListener('click', () => {
+    //     deliverDelayHost.querySelector('.spinner').classList.remove('d-none');
+    //     deliverDelayHost.querySelector('#thank-you-dialog').close();
+    //     deliverDelayHost.querySelector('#complain').close();
+    //     deliverDelayHost.querySelector('.layer').style.opacity = 0;
+    //     deliverDelayHost.querySelector('.layer').style.zIndex = -10;
 
-        const handleSpinner = setTimeout(() => {
-            deliverDelayHost.querySelector('.spinner').classList.add('d-none');
-            deliverDelayHost.querySelectorAll('.why-delay').forEach(el => el.classList.remove('d-none'));
-            clearTimeout(handleSpinner);
-        },500)
+    //     const handleSpinner = setTimeout(() => {
+    //         deliverDelayHost.querySelector('.spinner').classList.add('d-none');
+    //         deliverDelayHost.querySelectorAll('.why-delay').forEach(el => el.classList.remove('d-none'));
+    //         clearTimeout(handleSpinner);
+    //     },500)
 
 
-        const complainBtn = deliverDelayHost.querySelector('.why-click-here');
-        complainBtn.addEventListener('click', () => {
-            deliverDelayHost.querySelector('.spinner').classList.remove('d-none');
-            const finalDelay = setTimeout(() => {
-                deliverDelayHost.querySelector('.spinner').classList.add('d-none');
-                deliverDelayHost.querySelector('#complain').show();
-                deliverDelayHost.querySelector('.layer').style.opacity = 1;
-                deliverDelayHost.querySelector('.layer').style.zIndex = 1;
-                clearTimeout(finalDelay);
-            },500)
-        })
+    //     const complainBtn = deliverDelayHost.querySelector('.why-click-here');
+    //     complainBtn.addEventListener('click', () => {
+    //         deliverDelayHost.querySelector('.spinner').classList.remove('d-none');
+    //         const finalDelay = setTimeout(() => {
+    //             deliverDelayHost.querySelector('.spinner').classList.add('d-none');
+    //             deliverDelayHost.querySelector('#complain').show();
+    //             deliverDelayHost.querySelector('.layer').style.opacity = 1;
+    //             deliverDelayHost.querySelector('.layer').style.zIndex = 1;
+    //             clearTimeout(finalDelay);
+    //         },500)
+    //     })
        
         
-    })
+    // })
 })();
