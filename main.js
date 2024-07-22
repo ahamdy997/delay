12403e18aec50145bbfc2c81954cd031bf0ed81a
Ahmed Hamdy <bme.ahmedhamdy@gmail.com>
2024-07-22 21:08:47 +0300

    aaa

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
-(function () {
-    const deliverDelayHost = document.querySelector('#shadow-root');
-    const thankYouDialog = deliverDelayHost.querySelector('#thank-you-dialog');
+const deliverDelayHost = document.querySelector('#shadow-root');
+const thankYouDialog = deliverDelayHost.querySelector('#thank-you-dialog');
+const complainDialog = deliverDelayHost.querySelector('#complain');
 
-   
+(function () {
     const firstDelay = setTimeout(() => {
         deliverDelayHost.querySelector('.container').classList.remove('d-none');
         deliverDelayHost.querySelector('.spinner').remove();
         thankYouDialog.showModal();
         clearTimeout(firstDelay);
     },1000);
-
- 
 })();
-const deliverDelayHost = document.querySelector('#shadow-root');
-const complainDialog = deliverDelayHost.querySelector('#complain');
-const thankYouDialog = deliverDelayHost.querySelector('#thank-you-dialog');
 
 thankYouDialog.addEventListener('click', (e) => {
     if(!e.target.classList.contains('dialog-design')) thankYouDialog.close();
