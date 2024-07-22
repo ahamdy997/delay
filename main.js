ccc7a1d3de75a42c30731be3cd30d87e273234d8
Ahmed Hamdy <bme.ahmedhamdy@gmail.com>
2024-07-22 15:38:56 +0300

    dialog animation

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 (function () {
     const deliverDelayHost = document.querySelector('#shadow-root');
     const thankYouDialog = deliverDelayHost.querySelector('#thank-you-dialog');
-    const complainDialog = deliverDelayHost.querySelector('#complain');
 
-    thankYouDialog.addEventListener('click', (e) => {
-        if(!e.target.classList.contains('dialog-design')) thankYouDialog.close();
-    })
-
-    complainDialog.addEventListener('click', (e) => {
-        if(!e.target.classList.contains('dialog-design')) complainDialog.close();
-    })
+   
     const firstDelay = setTimeout(() => {
         deliverDelayHost.querySelector('.container').classList.remove('d-none');
         deliverDelayHost.querySelector('.spinner').classList.add('d-none');
@@ -17,10 +10,20 @@
         clearTimeout(firstDelay);
     },1000);
 
-    deliverDelayHost.querySelector('.why-click-here').addEventListener('click', () => {
-        complainDialog.showModal();
-    })
-
-    
  
 })();
+const deliverDelayHost = document.querySelector('#shadow-root');
+const complainDialog = deliverDelayHost.querySelector('#complain');
+const thankYouDialog = deliverDelayHost.querySelector('#thank-you-dialog');
+
+thankYouDialog.addEventListener('click', (e) => {
+    if(!e.target.classList.contains('dialog-design')) thankYouDialog.close();
+})
+
+complainDialog.addEventListener('click', (e) => {
+    if(!e.target.classList.contains('dialog-design')) complainDialog.close();
+})
+
+deliverDelayHost.querySelector('.why-click-here').addEventListener('click', () => {
+    complainDialog.showModal();
+})
